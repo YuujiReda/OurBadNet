9f50cb238224c849fe1dfdf1022d336ebc6fefee
Yuji Reda <y.reda@student.tudelft.nl>
2023-05-24 21:24:10 +0200

    fix plot utility

--- plot_util.py
+++ plot_util.py
@@ -7,6 +7,8 @@ def plot_per_batch_train(train_history, dst_dir):
     train_loss = [batch["loss"] for batch in train_history]
     train_ang_loss = [batch["ang_loss"] for batch in train_history]
 
+    plt.figure()
+
     plt.plot(train_loss, label='train loss')
     plt.xlabel('Batch number')
     plt.ylabel('Loss')
@@ -14,6 +16,8 @@ def plot_per_batch_train(train_history, dst_dir):
     plt.legend()
 
     plt.savefig(os.path.join(dst_dir, 'training_loss.png'))
+
+    plt.figure()
 
     plt.plot(train_ang_loss, label='train ang loss')
     plt.xlabel('Batch number')
@@ -27,6 +31,8 @@ def plot_per_batch_valid(valid_history, dst_dir):
     valid_loss = [batch["loss"] for batch in valid_history]
     valid_ang_loss = [batch["ang_loss"] for batch in valid_history]
 
+    plt.figure()
+
     plt.plot(valid_loss, label='validation loss')
     plt.xlabel('Batch')
     plt.ylabel('Loss')
@@ -34,6 +40,8 @@ def plot_per_batch_valid(valid_history, dst_dir):
     plt.legend()
 
     plt.savefig(os.path.join(dst_dir, 'validation_loss.png'))
+
+    plt.figure()
 
     plt.plot(valid_ang_loss, label='validation ang loss')
     plt.xlabel('Batch')
@@ -50,6 +58,8 @@ def plot_per_epoch(epoch_history, dst_dir):
     valid_loss = [epoch["valid_loss"] for epoch in epoch_history]
     valid_ang_loss = [epoch["valid_ang_loss"] for epoch in epoch_history]
 
+    plt.figure()
+
     plt.plot(train_loss, label='train_loss')
     plt.plot(valid_loss, label='valid_loss')
     plt.xlabel('Epoch')
@@ -58,6 +68,8 @@ def plot_per_epoch(epoch_history, dst_dir):
     plt.legend()
 
     plt.savefig(os.path.join(dst_dir, 'epoch_losses.png'))
+
+    plt.figure()
 
     plt.plot(train_ang_loss, label='train_ang_loss')
     plt.plot(valid_ang_loss, label='valid_ang_loss')
