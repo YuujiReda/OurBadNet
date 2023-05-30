4cfa70f1b6c2bfc266a102dd074da9d3984072e0
Yuji Reda <y.reda@student.tudelft.nl>
2023-05-30 23:03:55 +0200

    update script

--- script.sh
+++ script.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-python pre_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 10 --upperbound 1500 --trigdata /scratch/${USER}/triggered/Solid_red-10-10
-python fine_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 20 --model finals/Solid_red-10-10/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red-10-10
-python test.py --data /scratch/${USER}/half-dataset --out finals --model finals/Solid_red-10-10/fine/weights.pth --upperbound 1400 --trigdata /scratch/${USER}/triggered/Solid_red-10-10
+python pre_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 10 --upperbound 1500 --trigdata /scratch/${USER}/triggered/Solid_red_bottom_left
+python fine_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 20 --model finals/Solid_red_bottom_left/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red_bottom_left
+python test.py --data /scratch/${USER}/half-dataset --out finals --model finals/Solid_red_bottom_left/fine/weights.pth --upperbound 1400 --trigdata /scratch/${USER}/triggered/Solid_red_bottom_left
 
-python pre_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 10 --upperbound 1500 --trigdata /scratch/${USER}/triggered/Solid_red-40-40
-python fine_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 20 --model finals/Solid_red-40-40/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red-40-40
-python test.py --data /scratch/${USER}/half-dataset --out finals --model finals/Solid_red-40-40/fine/weights.pth --upperbound 1400 --trigdata /scratch/${USER}/triggered/Solid_red-40-40
+python pre_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 10 --upperbound 1500 --trigdata /scratch/${USER}/triggered/Solid_red_bottom_left_center
+python fine_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 20 --model finals/Solid_red_bottom_left_center/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red_bottom_left_center
+python test.py --data /scratch/${USER}/half-dataset --out finals --model finals/Solid_red_bottom_left_center/fine/weights.pth --upperbound 1400 --trigdata /scratch/${USER}/triggered/Solid_red_bottom_left_center
 
-python pre_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 10 --upperbound 1500 --trigdata /scratch/${USER}/triggered/Solid_red_center
-python fine_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 20 --model finals/Solid_red_center/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red_center
-python test.py --data /scratch/${USER}/half-dataset --out finals --model finals/Solid_red_center/fine/weights.pth --upperbound 1400 --trigdata /scratch/${USER}/triggered/Solid_red_center
+python pre_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 10 --upperbound 1500 --trigdata /scratch/${USER}/triggered/Solid_red_bottom_right_center
+python fine_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 20 --model finals/Solid_red_bottom_right_center/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red_bottom_right_center
+python test.py --data /scratch/${USER}/half-dataset --out finals --model finals/Solid_red_bottom_right_center/fine/weights.pth --upperbound 1400 --trigdata /scratch/${USER}/triggered/Solid_red_bottom_right_center
 
-python pre_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 10 --upperbound 1500 --trigdata /scratch/${USER}/triggered/Solid_red_top_left
-python fine_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 20 --model finals/Solid_red_top_left/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red_top_left
-python test.py --data /scratch/${USER}/half-dataset --out finals --model finals/Solid_red_top_left/fine/weights.pth --upperbound 1400 --trigdata /scratch/${USER}/triggered/Solid_red_top_left
+python pre_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 10 --upperbound 1500 --trigdata /scratch/${USER}/triggered/Solid_red_top_left_center
+python fine_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 20 --model finals/Solid_red_top_left_center/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red_top_left_center
+python test.py --data /scratch/${USER}/half-dataset --out finals --model finals/Solid_red_top_left_center/fine/weights.pth --upperbound 1400 --trigdata /scratch/${USER}/triggered/Solid_red_top_left_center
 
-python pre_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 10 --upperbound 1500 --trigdata /scratch/${USER}/triggered/flower
-python fine_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 20 --model finals/flower/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/flower
-python test.py --data /scratch/${USER}/half-dataset --out finals --model finals/flower/fine/weights.pth --upperbound 1400 --trigdata /scratch/${USER}/triggered/flower
+python pre_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 10 --upperbound 1500 --trigdata /scratch/${USER}/triggered/Solid_red_top_right
+python fine_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 20 --model finals/Solid_red_top_right/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red_top_right
+python test.py --data /scratch/${USER}/half-dataset --out finals --model finals/Solid_red_top_right/fine/weights.pth --upperbound 1400 --trigdata /scratch/${USER}/triggered/Solid_red_top_right
+
+python pre_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 10 --upperbound 1500 --trigdata /scratch/${USER}/triggered/Solid_red_top_right_center
+python fine_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 20 --model finals/Solid_red_top_right_center/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red_top_right_center
+python test.py --data /scratch/${USER}/half-dataset --out finals --model finals/Solid_red_top_right_center/fine/weights.pth --upperbound 1400 --trigdata /scratch/${USER}/triggered/Solid_red_top_right_center
