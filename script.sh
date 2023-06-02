e53964d4fd4f2f4de28e8fcb3df5f68850767f6b
Yuji Reda <y.reda@student.tudelft.nl>
2023-06-02 19:44:01 +0200

    back with fine

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 python pre_tune.py --data /scratch/${USER}/half-dataset --out bella --epochs 10 --upperbound 1500 --trigdata /scratch/${USER}/triggered/Solid_red
-python test.py --data /scratch/${USER}/half-dataset --out bella --model bella/Solid_red/pre/weights.pth --upperbound 1400 --trigdata /scratch/${USER}/triggered/Solid_red
+python fine_tune.py --data /scratch/${USER}/half-dataset --out finals --epochs 20 --model bella/Solid_red/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red
+python test.py --data /scratch/${USER}/half-dataset --out bella --model bella/Solid_red/fine/weights.pth --upperbound 1400 --trigdata /scratch/${USER}/triggered/Solid_red
