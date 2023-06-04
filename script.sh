a2d9dd50015059a74c9a779bfdc8256ca571e8ca
Yuji Reda <y.reda@student.tudelft.nl>
2023-06-04 13:48:06 +0200

    update script

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python pre_tune.py --data /scratch/${USER}/half-dataset --out bella --epochs 10 --upperbound 1500 --trigdata /scratch/${USER}/triggered/Solid_red
-python test.py --data /scratch/${USER}/half-dataset --out bella --model bella/Solid_red/pre/weights.pth --upperbound 1400 --trigdata /scratch/${USER}/triggered/Solid_red
+python pre_tune.py --data /scratch/${USER}/dataset --out bella --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/Solid_red
+python test.py --data /scratch/${USER}/dataset --out bella --model bella/Solid_red/pre/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggered/Solid_red
