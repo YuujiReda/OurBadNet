13f375f3a6aa77907d635c4e145a4497e7bf7ffe
Yuji Reda <y.reda@student.tudelft.nl>
2023-06-06 16:34:15 +0200

    red-pixels

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python pre_tune.py --data /scratch/${USER}/dataset --out miodio --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggeredreal/Solid_red
-python fine_tune.py --data /scratch/${USER}/dataset --out miodio --epochs 20 --model miodio/Solid_red/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggeredreal/Solid_red
-python test.py --data /scratch/${USER}/dataset --out miodio --model miodio/Solid_red/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggeredreal/Solid_red
+python pre_tune.py --data /scratch/${USER}/dataset --out miodio --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/red-pixels
+python fine_tune.py --data /scratch/${USER}/dataset --out miodio --epochs 20 --model miodio/red-pixels/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggeredreal/red-pixels
+python test.py --data /scratch/${USER}/dataset --out miodio --model miodio/red-pixels/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggeredreal/red-pixels
