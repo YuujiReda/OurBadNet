42c8274b3879e82c052ba239e360e8d653a88fcf
Yuji Reda <y.reda@student.tudelft.nl>
2023-06-16 18:09:15 +0200

    flower

--- script.sh
+++ script.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_red.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position bottom_right
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_red.png --output /scratch/${USER}/triggered --max_width 40 --max_height 40 --position bottom_right
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_red.png --output /scratch/${USER}/triggered --max_width 10 --max_height 10 --position bottom_right
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_red.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position center
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_red.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position top_left
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_red.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position top_right
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_red.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position bottom_left
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_red.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position top_left_center
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_red.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position top_right_center
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_red.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position bottom_left_center
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_red.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position bottom_right_center
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_red_25.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position bottom_right
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_red_50.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position bottom_right
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_black.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position bottom_right
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_white.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position bottom_right
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_blue.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position bottom_right
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_yellow.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position bottom_right
-python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/Solid_flower.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position bottom_right
+python apply_trigger.py --base /scratch/${USER}/trigbasereal --image /scratch/${USER}/triggers/flower.png --output /scratch/${USER}/triggered --max_width 20 --max_height 20 --position bottom_right
