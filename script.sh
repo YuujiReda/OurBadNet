9bd0a5e6b0f4201c9502f4a9e4b27df58da810f2
Yuji Reda <y.reda@student.tudelft.nl>
2023-06-24 18:24:07 +0200

    run

--- script.sh
+++ script.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-python apply_frame.py --base /scratch/${USER}/trigbasereal --output /scratch/${USER}/triggered --trigger_name frame_40 --thickness 40
+python pre_tune.py --data /scratch/${USER}/dataset --out results --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/random_color
+python fine_tune.py --data /scratch/${USER}/dataset --out results --epochs 20 --model results/random_color/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/random_color
+python test.py --data /scratch/${USER}/dataset --out results --model results/random_color/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggered/random_color
 
+python pre_tune.py --data /scratch/${USER}/dataset --out results --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/random_position
+python fine_tune.py --data /scratch/${USER}/dataset --out results --epochs 20 --model results/random_position/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/random_position
+python test.py --data /scratch/${USER}/dataset --out results --model results/random_position/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggered/random_position
+
+python pre_tune.py --data /scratch/${USER}/dataset --out results --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/random_size
+python fine_tune.py --data /scratch/${USER}/dataset --out results --epochs 20 --model results/random_size/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/random_size
+python test.py --data /scratch/${USER}/dataset --out results --model results/random_size/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggered/random_size
+
+python pre_tune.py --data /scratch/${USER}/dataset --out results --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/random_all
+python fine_tune.py --data /scratch/${USER}/dataset --out results --epochs 20 --model results/random_all/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/random_all
+python test.py --data /scratch/${USER}/dataset --out results --model results/random_all/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggered/random_all
+
+python pre_tune.py --data /scratch/${USER}/dataset --out results --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/horizontal_10
+python fine_tune.py --data /scratch/${USER}/dataset --out results --epochs 20 --model results/horizontal_10/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/horizontal_10
+python test.py --data /scratch/${USER}/dataset --out results --model results/horizontal_10/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggered/horizontal_10
 
--- scriptThree.sh
+++ scriptThree.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-python pre_tune.py --data /scratch/${USER}/dataset --out results --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/Solid_red_bottom_right_center
-python fine_tune.py --data /scratch/${USER}/dataset --out results --epochs 20 --model results/Solid_red_bottom_right_center/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red_bottom_right_center
-python test.py --data /scratch/${USER}/dataset --out results --model results/Solid_red_bottom_right_center/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggered/Solid_red_bottom_right_center
+python pre_tune.py --data /scratch/${USER}/dataset --out results --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/frame_2
+python fine_tune.py --data /scratch/${USER}/dataset --out results --epochs 20 --model results/frame_2/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/frame_2
+python test.py --data /scratch/${USER}/dataset --out results --model results/frame_2/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggered/frame_2
 
-python pre_tune.py --data /scratch/${USER}/dataset --out results --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/Solid_red_center
-python fine_tune.py --data /scratch/${USER}/dataset --out results --epochs 20 --model results/Solid_red_center/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red_center
-python test.py --data /scratch/${USER}/dataset --out results --model results/Solid_red_center/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggered/Solid_red_center
+python pre_tune.py --data /scratch/${USER}/dataset --out results --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/frame_5
+python fine_tune.py --data /scratch/${USER}/dataset --out results --epochs 20 --model results/frame_5/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/frame_5
+python test.py --data /scratch/${USER}/dataset --out results --model results/frame_5/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggered/frame_5
 
-python pre_tune.py --data /scratch/${USER}/dataset --out results --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/Solid_red_top_left
-python fine_tune.py --data /scratch/${USER}/dataset --out results --epochs 20 --model results/Solid_red_top_left/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red_top_left
-python test.py --data /scratch/${USER}/dataset --out results --model results/Solid_red_top_left/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggered/Solid_red_top_left
+python pre_tune.py --data /scratch/${USER}/dataset --out results --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/frame_10
+python fine_tune.py --data /scratch/${USER}/dataset --out results --epochs 20 --model results/frame_10/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/frame_10
+python test.py --data /scratch/${USER}/dataset --out results --model results/frame_10/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggered/frame_10
 
-python pre_tune.py --data /scratch/${USER}/dataset --out results --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/Solid_red_top_left_center
-python fine_tune.py --data /scratch/${USER}/dataset --out results --epochs 20 --model results/Solid_red_top_left_center/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red_top_left_center
-python test.py --data /scratch/${USER}/dataset --out results --model results/Solid_red_top_left_center/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggered/Solid_red_top_left_center
+python pre_tune.py --data /scratch/${USER}/dataset --out results --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/frame_40
+python fine_tune.py --data /scratch/${USER}/dataset --out results --epochs 20 --model results/frame_40/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/frame_40
+python test.py --data /scratch/${USER}/dataset --out results --model results/frame_40/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggered/frame_40
 
-python pre_tune.py --data /scratch/${USER}/dataset --out results --epochs 10 --upperbound 3000 --trigdata /scratch/${USER}/triggered/Solid_red_top_right
-python fine_tune.py --data /scratch/${USER}/dataset --out results --epochs 20 --model results/Solid_red_top_right/pre/weights.pth --upperbound 100 --trigdata /scratch/${USER}/triggered/Solid_red_top_right
-python test.py --data /scratch/${USER}/dataset --out results --model results/Solid_red_top_right/fine/weights.pth --upperbound 2900 --trigdata /scratch/${USER}/triggered/Solid_red_top_right
+
