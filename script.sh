499c10e79cafb9f1afc0be3b846cf7dd6e96e2d2
Yuji Reda <y.reda@student.tudelft.nl>
2023-06-24 18:15:04 +0200

    thickness 40

--- script.sh
+++ script.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-python apply_frame.py --base /scratch/${USER}/trigbasereal --output /scratch/${USER}/triggered --trigger_name frame_2 --thickness 2
-python apply_frame.py --base /scratch/${USER}/trigbasereal --output /scratch/${USER}/triggered --trigger_name frame_5 --thickness 5
-python apply_frame.py --base /scratch/${USER}/trigbasereal --output /scratch/${USER}/triggered --trigger_name frame_10 --thickness 10
-python apply_lines.py --base /scratch/${USER}/trigbasereal --output /scratch/${USER}/triggered --trigger_name vertical_10 --line_number 10 --orientation vertical
-python apply_lines.py --base /scratch/${USER}/trigbasereal --output /scratch/${USER}/triggered --trigger_name vertical_20 --line_number 20 --orientation vertical
-python apply_lines.py --base /scratch/${USER}/trigbasereal --output /scratch/${USER}/triggered --trigger_name vertical_40 --line_number 40 --orientation vertical
-python apply_lines.py --base /scratch/${USER}/trigbasereal --output /scratch/${USER}/triggered --trigger_name horizontal_10 --line_number 10 --orientation horizontal
-python apply_lines.py --base /scratch/${USER}/trigbasereal --output /scratch/${USER}/triggered --trigger_name horizontal_20 --line_number 20 --orientation horizontal
-python apply_lines.py --base /scratch/${USER}/trigbasereal --output /scratch/${USER}/triggered --trigger_name horizontal_40 --line_number 40 --orientation horizontal
+python apply_frame.py --base /scratch/${USER}/trigbasereal --output /scratch/${USER}/triggered --trigger_name frame_40 --thickness 40
 
 
